c777ad3b50e7be392a1e4b31ca0d6fa2327dd6a2
4w1q <iuygarkara@gmail.com>
2025-07-08 02:06:09 +0300

    Update keep_alive.py
    Signed-off-by: 4w1q <iuygarkara@gmail.com>

--- keep_alive.py
+++ keep_alive.py
@@ -1,19 +1,17 @@
+# keep_alive.py
 from flask import Flask
 from threading import Thread
+import os
 
-app = Flask('')
+app = Flask(__name__)
 
-
-@app.route('/')
+@app.route("/")
 def home():
-  return "I'm alive"
+    return "👍 Bot ayakta", 200
 
-
-def run():
-  app.run(host='0.0.0.0', port=8080)
-
+def _run():
+    port = int(os.environ.get("PORT", 8080))  # Render PORT'u otomatik verir
+    app.run(host="0.0.0.0", port=port)
 
 def keep_alive():
-  t = Thread(target=run)
-  t.daemon = True
-  t.start()
+    Thread(target=_run, daemon=True).start()
